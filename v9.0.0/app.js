dd513f9dca4c163d8a55902b9014e3514037c676
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-12-27 11:25:55 +0530

    added authentication and edit delete for comments

--- v9.0.0/app.js
+++ v9.0.0/app.js
@@ -19,7 +19,7 @@ const express =                         require("express"),
 
 const app = express();
 app.set("view engine","ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(expressSession({
   secret : "Aniket is a good guy ",
